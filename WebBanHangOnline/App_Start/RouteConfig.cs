d73ddbb1fedbbefb7513cc4ca5818ee0a0e52643
wwoi03 <tuandao8826848@gmail.com>
2023-11-24 19:29:03 +0700

    add template admin: nav menu

--- WebBanHangOnline/App_Start/RouteConfig.cs
+++ WebBanHangOnline/App_Start/RouteConfig.cs
@@ -16,7 +16,9 @@ namespace WebBanHangOnline
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "WebBanHangOnline.Controllers" }
+                //namespaces: new[] { "WebBanHangOnline.Areas.Admin.Controllers" }
             );
         }
     }
--- WebBanHangOnline/Areas/Admin/AdminAreaRegistration.cs
+++ WebBanHangOnline/Areas/Admin/AdminAreaRegistration.cs
@@ -17,7 +17,8 @@ namespace WebBanHangOnline.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "WebBanHangOnline.Areas.Admin.Controllers" }
             );
         }
     }
